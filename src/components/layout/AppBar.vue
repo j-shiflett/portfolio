4029a328ed52a14f5e019c9205dc48fb2b08c0da
James Shiflett <jmshiflett92@gmail.com>
2020-06-13 23:39:02 -0500

    updated appbar

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-card class="overflow-hidden">
+  <v-card>
     <v-app-bar app color="white" flat>
-      <v-toolbar-title>James Shiflett</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn v-for="link in links" :key="link.name" :to="link.path" text>{{ link.name }}</v-btn>
     </v-app-bar>
