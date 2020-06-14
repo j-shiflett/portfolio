6fecfd1fd532e7ea919bed8e1854f827599b3b41
James Shiflett <jmshiflett92@gmail.com>
2020-06-13 23:37:08 -0500

    Initial appbar built

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar app color="white" flat>
+      <v-toolbar-title>James Shiflett</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-for="link in links" :key="link.name" :to="link.path" text>{{ link.name }}</v-btn>
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  data: () => ({
+    links: [
+      {
+        name: "Home",
+        path: "/"
+      },
+      {
+        name: "About Me",
+        path: "/aboutme"
+      },
+      {
+        name: "Projects",
+        path: "/projects"
+      },
+      {
+        name: "Contact Me",
+        path: "/contactme"
+      }
+    ]
+  })
+};
+</script>
