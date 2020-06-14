e4e47061be4099ecdc43d1bd4ded3480b38df104
James Shiflett <jmshiflett92@gmail.com>
2020-06-13 22:31:30 -0500

    added vuefire to main.js

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+import firebase from "firebase";
+import { firestorePlugin } from "vuefire";
+
+Vue.use(firestorePlugin);
+
+export const db = firebase.firestore();
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
